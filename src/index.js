ae9e01d28ae8e67547cced9023f8c5b48c01b6df
Deepak Kumar <dk1407610@gmail.com>
2025-09-09 10:14:17 +0530

    added tweet service with hashtag processing

--- src/index.js
+++ src/index.js
@@ -7,4 +7,5 @@ app.listen(3000, async ()=>{
     console.log('server started');
     await connect();
     console.log('Mongo db connected');
+    let service = new TweetService();
 })
---
+++ src/repository/hashtag-repository.js
@@ -0,0 +1,62 @@
+const Hashtag = require('../models/hashtags');
+
+class HashtagRepository{
+
+    async create(data){
+        try {
+            const tag = await Hashtag.create(data);
+            return tag;
+        } catch (error) {
+            console.log(error);
+            // It's good practice to re-throw the error so the calling
+            // function can handle it.
+            throw error; 
+        }
+    }
+
+    async bulkCreate(data){
+        try {
+            const tags = await Hashtag.insertMany(data);
+            return tags; // You should return the result of insertMany
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+    }
+
+    async get(id){
+        try {
+            const tag = await Hashtag.findById(id);
+            return tag;
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+    }
+
+    async destroy(id){
+        try {
+            const response = await Hashtag.findByIdAndRemove(id);
+            return response;
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+    }
+
+    async findByName(titleList){
+        try {
+            const tags = await Hashtag.find({
+                title: titleList // Use $in for efficient lookup of multiple titles
+            });
+            return tags;
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+    }
+
+    
+}
+
+module.exports=HashtagRepository;
